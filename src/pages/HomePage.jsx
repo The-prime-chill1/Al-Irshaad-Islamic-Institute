7fe776bea8cfa00c7d7beef4c78cbefb9cf82902
The-prime-chill1 <lamidiabdulhameedololawale@gmail.com>
2026-09-14 23:39:21 +0100

    about page upbdate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import HeroSection from '../components/sections/HeroSection';
 import HadithRibbon from '../components/common/HadithRibbon';
-import ProgramsGridSection from '../components/sections/ProgramsGridSection';
 import WhyUsSection from '../components/sections/WhyUsSection';
 import HowItWorksSection from '../components/sections/HowItWorksSection';
 import TestimonialsSection from '../components/sections/TestimonialsSection';
@@ -17,22 +16,19 @@ export default function HomePage() {
       {/* 2. Sacred Hadith Ribbon */}
       <HadithRibbon />
 
-      {/* 3. Structured Learning Programs (2 Categories + Discover More Categories CTA) */}
-      <ProgramsGridSection initialCategoryCount={2} />
-
-      {/* 4. Key Advantages & Live Online Experience */}
+      {/* 3. Key Advantages & Live Online Experience */}
       <WhyUsSection />
 
-      {/* 5. How Online Learning Works (Simple 3-Step Process) */}
+      {/* 4. How Online Learning Works (Simple 3-Step Process) */}
       <HowItWorksSection />
 
-      {/* 6. What Our Students & Parents Say (Testimonials) */}
+      {/* 5. What Our Students & Parents Say (Testimonials) */}
       <TestimonialsSection />
 
-      {/* 7. Frequently Asked Questions */}
+      {/* 6. Frequently Asked Questions */}
       <FaqAccordionSection limit={4} />
 
-      {/* 8. Final Call to Action Banner */}
+      {/* 7. Final Call to Action Banner */}
       <CtaBannerSection />
     </div>
   );
